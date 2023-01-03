a9b7c31ec4406ca4c266a717d9771a321b384bd2
tuankietha <106296783+tuankietha@users.noreply.github.com>
2023-01-03 12:10:02 +0700

    update

--- components/Home/Home1.vue
+++ components/Home/Home1.vue
@@ -10,30 +10,21 @@ import { TagCloud } from "../../utils/TagCloud"
 export default {
     data() {
         return {
-        headers: [
-            { imgs: "/slile1.jpg", link: "https://www.facebook.com/" },
-            { imgs: "/slile2.jpg", link: "https://www.facebook.com/" },
-            { imgs: "/slile3.jpg", link: "https://www.facebook.com/" },
-            { imgs: "/slile4.jpg", link: "https://www.facebook.com/" },
-            { imgs: "/slile5.jpg", link: "https://www.facebook.com/" },
-            { imgs: "/slile1.jpg", link: "https://www.facebook.com/" },
-            { imgs: "/slile2.jpg", link: "https://www.facebook.com/" },
-            { imgs: "/slile3.jpg", link: "https://www.stiq.com/" },
-        ],
         stacks: [
-            "/slile3.jpg",
-            "/slile2.jpg",
-            "/slile1.jpg",
-            "/slile3.jpg",
-            "/slile4.jpg",
-            "/slile2.jpg",
-            "/slile5.jpg",
-            "/slile2.jpg",
-            "/slile6.jpg",
-            "/slile8.jpg",
-            "/slile9.jpg",
-            "/slile7.jpg",
-            "/slile1.jpg",
+            "/slideplogg/slide.png",
+            "/slideplogg/slide2.png",
+            "/slideplogg/slide3.png",
+            "/slideplogg/slide4.png",
+            "/slideplogg/slide5.png",
+            "/slideplogg/slide6.png",
+            "/slideplogg/slide7.png",
+            "/slideplogg/slide8.png",
+            "/slideplogg/slide.png",
+            "/slideplogg/slide1.png",
+            "/slideplogg/slide2.png",
+            "/slideplogg/slide3.png",
+            "/slideplogg/slide4.png"
+
         ],
         current: 1,
         interval: undefined,
@@ -62,7 +53,7 @@ export default {
     mounted(){
         const container = '.content';
         const options = {
-            radius: 400,
+            radius: 500,
             keep: false,
             maxSpeed: "fast"
         };
@@ -74,7 +65,7 @@ export default {
 
 <style scoped >
     .container {
-        display: flex;  
+        display: flex;
         justify-content: center;
     }
 </style>
