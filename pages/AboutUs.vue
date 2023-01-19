9895f5a72e2cf99678658312980ffffaf60955f5
tuankietha <106296783+tuankietha@users.noreply.github.com>
2023-01-19 22:52:54 +0700

    fix bug

--- pages/AboutUs.vue
+++ pages/AboutUs.vue
@@ -1,235 +1,59 @@
 <template>
 	<v-sheet>
-		<v-card
-			flat
-			color="transparent"
-			:height="$vuetify.breakpoint.smAndDown ? '' : '80 '"
-		></v-card>
 		<v-layout
 			:class="$vuetify.breakpoint.smAndDown ? '' : 'py-5 px-16 mx-15'"
 		>
-			<template>
-				<div>
-					<v-col>
-						<v-layout
-							justify-center
-							data-aos="fade-down"
-							v-if="$vuetify.breakpoint.smAndDown"
-						>
-							<img
-								contain
-								aspect-ratio="1"
-								:width="
-									$vuetify.breakpoint.smAndDown ? '200' : ''
-								"
-								:class="
-									$vuetify.breakpoint.smAndDown
-										? 'top-side justify-center'
-										: ''
-								"
-								src="/logowe.jpg"
-							/>
-						</v-layout>
-					</v-col>
-
-					<v-col md="9" v-if="$vuetify.breakpoint.mdAndUp">
-						<img
-							contain
-							aspect-ratio="1"
-							:width="$vuetify.breakpoint.smAndDown ? ' 200' : ''"
-							:class="
-								$vuetify.breakpoint.smAndDown ? '' : 'left-side'
-							"
-							src="/logowe.jpg"
-						/>
-					</v-col>
-
-					<div
-						data-aos="fade-down"
-						data-aos-duration="900"
-						:class="
-							$vuetify.breakpoint.smAndDown
-								? 'h6 text-center font-weight-bold '
-								: 'h4 pl-10 text-wrap font-weight-bold'
-						"
-					>
-						{{ $t("title.1") }}
-					</div>
-
-					<div
-						data-aos="fade-down-right"
-						data-aos-duration="900"
-						class="content"
-					>
-						<p
-							:class="
-								$vuetify.breakpoint.smAndDown
-									? 'text-center grey--text subtitle-1 mx-6'
-									: ' h6 grey--text mt-10 ma-12'
-							"
-						>
-							{{ $t("title.2") }}
-						</p>
-					</div>
-				</div>
-			</template>
+			<about-us-text-item-vue/>
 		</v-layout>
 
 		<v-img
+			data-aos="flip-up"
+			data-aos-duration="500"
 			contain
 			:width="$vuetify.breakpoint.smAndDown ? '100%' : '100vw'"
 			src="/teamplogg.jpg"
 		/>
 		<v-sheet :class="$vuetify.breakpoint.smAndDown ? 'mx-5' : 'px-16'">
-			<template>
-				<div
-					v-for="(head, index) in heads"
-					:key="index"
-					:class="head.class"
-				>
-					<v-col :cols="head.col">
-						<div>
-							{{ head.text }}
-						</div>
+			<template >
+				<div data-aos="zoom-in" data-aos-duration="900">
+					<div
+					class="mt-16"
+						:class="$vuetify.breakpoint.smAndDown
+						?'text-center text-h4 font-weight-bold'
+						:'text-h2 font-weight-bold'">
+							{{ $t("ploggteam") }}
+					</div>
+					<v-col md="11"
+						:class="$vuetify.breakpoint.smAndDown
+						?'text-center subtitle-1 grey--text'
+						:'text-h5 grey--text'">
+							{{ $t("form.1") }}
 					</v-col>
-				</div>
+					</div>
 			</template>
 
 			<template>
-				<v-sheet
-					class="d-flex flex-column flex-md-row align-center justify-center my-9 mt-16"
-				>
-					<v-img
-						:max-width="
-							$vuetify.breakpoint.smAndDown ? '60%' : '35%'
-						"
-						contain
-						src="/OurServices//service.jpg"
-					/>
-
+				<div  data-aos="zoom-in" data-aos-duration="500">
 					<div
-						:class="[
-							'd-flex flex-column',
-							$vuetify.breakpoint.smAndDown
-								? 'black--text'
-								: 'black--text align-end w-55 ',
-						]"
-					>
-						<div
-							:class="
-								$vuetify.breakpoint.smAndDown
-									? 'h5  text-center font-weight-bold'
-									: 'h3 font-weight-bold'
-							"
-						>
-							{{ $t("title.7") }}
-						</div>
-
-						<div
-							:class="
-								$vuetify.breakpoint.smAndDown
-									? 'mt-2  text-center grey--text'
-									: 'h6 text-end pt-2 ml-12 grey--text'
-							"
-						>
-							{{ $t("title.8") }}
-						</div>
+					class="mt-16"
+						:class="$vuetify.breakpoint.smAndDown
+						?'text-h4 text-center font-weight-bold'
+						:'text-h2 font-weight-bold'">
+							{{ $t("ourmission") }}
 					</div>
-				</v-sheet>
+					<v-col md="9"
+						:class="$vuetify.breakpoint.smAndDown
+						?'text-center subtitle-1 grey--text'
+						:'text-h5 grey--text'">
+							{{ $t("form.2") }}
+					</v-col>
+					</div>
 			</template>
 
-			<template>
-				<v-sheet
-					:class="
-						$vuetify.breakpoint.smAndDown
-							? 'mx-2'
-							: ' mx-8 pl-2 py-5'
-					"
-					max-width="100%"
-				>
-					<div
-						:class="
-							$vuetify.breakpoint.smAndDown
-								? 'h5 mt-2 font-weight-bold text-center'
-								: 'h3 justify-start font-weight-bold '
-						"
-					>
-						{{ $t("title.9") }}
-					</div>
-					<v-slide-group
-						center-active
-						show-arrows
-						v-model="current"
-						:class="
-							$vuetify.breakpoint.smAndDown ? '' : 'pa-5 mt-12'
-						"
-					>
-						<v-slide-item
-							disabled
-							v-for="(header, index) in headers"
-							:key="index"
-						>
-							<v-card elevation="0" height="200" width="400">
-								<v-img contain :src="header.imgs" />
-							</v-card>
-						</v-slide-item>
-					</v-slide-group>
-				</v-sheet>
-			</template>
-			<template>
-				<template>
-					<div
-						:class="
-							$vuetify.breakpoint.smAndDown
-								? 'h5 font-weight-bold text-center py-9'
-								: 'h3 justify-end pl-10 font-weight-bold '
-						"
-					>
-						{{ $t("title.10") }}
-					</div>
-					<v-row>
-						<v-hover v-for="(form, index) in forms" :key="index">
-							<template #default="{ hover }">
-								<v-card
-									:class="
-										$vuetify.breakpoint.smAndDown
-											? ''
-											: ' mx-auto ma-12 '
-									"
-									class="rounded-xl"
-								>
-									<v-img
-										contain
-										height="300px"
-										width="400px"
-										:src="form.imgs"
-									/>
-									<v-card-text
-										class="text-center font-weight-bold"
-									>
-										<h2 class="text-h5 black--text">
-											{{ form.Name }}
-										</h2>
-										<h3>{{ form.Position }}</h3>
-									</v-card-text>
+			<about-us-title-item/>
+			<about-us-slide-group/>
+			<about-us-out-member/>
 
-									<v-fade-transition>
-										<v-overlay
-											v-if="hover"
-											absolute
-											color="#036358"
-										>
-											<v-btn :href="form.links">{{
-												$t("see")
-											}}</v-btn>
-										</v-overlay>
-									</v-fade-transition>
-								</v-card>
-							</template>
-						</v-hover>
-					</v-row>
-				</template>
-			</template>
 		</v-sheet>
 	</v-sheet>
 </template>
@@ -238,153 +62,12 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 export default {
-	data() {
-		return {
-			forms: [
-				{
-					imgs: "/AI/ducanh.jpg",
-					Name: "Mr. Le Duc Anh ",
-					Position: "CEO",
-				},
-				{
-					imgs: "/AI/philippe.jpg",
-					Name: "Mr. Philippe Marchesseault ",
-					Position: "CEO",
-				},
-				{
-					imgs: "/AI/eddine.jpg",
-					Name: "Mr. Eddine Alimat ",
-					Position: "President Synode",
-				},
-				{
-					imgs: "/AI/maxine.jpg",
-					Name: "Mr. Maxime ",
-					Position: "Chief Technology Officer",
-				},
-				{
-					imgs: "/AI/Phạm Nguyễn Thuận.png",
-					Name: "Mr. Thuan Pham",
-					Position: this.$t("director"),
-				},
-				{
-					imgs: "/avt3.jpg",
-					Name: "Mr. Duy Minh",
-					Position: this.$t("develop"),
-				},
-
-				{
-					imgs: "/AI/Duy Quang.png",
-					Name: "Mr. Quang Duy",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Hà Tuấn Kiệt.png",
-					Name: "Mr. Kiet Ha ",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Đỗ Duy Khang.png",
-					Name: "Mr. Khang Do",
-					Position: this.$t("develop"),
-				},
-
-				{
-					imgs: "/AI/Đỗ Đăng Trình.png",
-					Name: "Mr. Trinh Do",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Dương Bảo Nam.png",
-					Name: "Mr. Nam Duong",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Tấn Đạt Lê.png",
-					Name: "Mr. Dat Le",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Thanh Sơn.png",
-					Name: "Mr. Son Thanh",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Trần Nguyễn Nhật Quang.png",
-					Name: "Mr. Quang Tran",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Nguyễn Quốc Hoàng Anh.png",
-					Name: "Mr. Anh Nguyen ",
-					Position: this.$t("develop"),
-				},
-				{
-					imgs: "/AI/Phạm Thanh Thảo.png",
-					Name: "Mr. Thao Pham ",
-					Position: this.$t("develop"),
-				},
-			],
-			headers: [
-				{ imgs: "/About_Us/item1.jpeg", link: "" },
-				{ imgs: "/About_Us/item2.jpeg", link: "" },
-				{ imgs: "/About_Us/item3.jpeg", link: "" },
-				{ imgs: "/About_Us/item4.jpeg", link: "" },
-				{ imgs: "/About_Us/item5.jpeg", link: "" },
-				{ imgs: "/About_Us/item6.jpeg", link: "" },
-				{ imgs: "/About_Us/item5.jpeg", link: "" },
-				{ imgs: "/About_Us/item8.jpeg", link: "" },
-			],
-			heads: [
-				{
-					text: this.$t("form.1"),
-					class: this.$vuetify.breakpoint.smAndDown
-						? "h5 font-weight-bold text-center mt-16"
-						: "h3 front-weight-black mt-16",
-				},
-				{
-					text: this.$t("form.2"),
-					class: this.$vuetify.breakpoint.smAndDown
-						? "subtitle-1 grey--text text-center"
-						: "h6 grey--text",
-				},
-				{
-					text: this.$t("form.3"),
-					class: this.$vuetify.breakpoint.smAndDown
-						? "h5 font-weight-bold text-center mt-16"
-						: "h3 front-weight-black mt-16",
-				},
-				{
-					text: this.$t("form.4"),
-					class: this.$vuetify.breakpoint.smAndDown
-						? "subtitle-1 grey--text text-center"
-						: "h6 grey--text",
-					col: this.$vuetify.breakpoint.smAndDown ? 12 : 8,
-				},
-			],
-			current: 1,
-			interval: undefined,
-			direction: 1,
-			slideInterval: 2,
-		};
-	},
-	created() {
-		this.interval = setInterval(() => {
-			if (
-				this.current === 0 ||
-				this.current === this.headers.length - 1
-			) {
-				this.direction *= -1;
-				this.current += this.direction;
-			} else {
-				this.current += this.direction;
-			}
-		}, this.slideInterval * 1000);
-	},
-	beforeDestroy() {
-		clearInterval(this.interval);
-	},
 	mounted() {
-		AOS.init();
+		AOS.init({
+  offset: 20,
+});
+		
+		
 	},
 };
 </script>
