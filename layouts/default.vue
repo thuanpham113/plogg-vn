c6595b1e0cb5affadbe8b02fb95a6a58365d66bd
Hai <106296783+tuankietha@users.noreply.github.com>
2023-01-31 13:15:35 +0700

    fix locale issues

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,212 +1,195 @@
 <template>
-	<v-app id="app" style="overflow:scroll">
-		<v-app-bar
-			elevation="60"
-			elevate-on-scroll
-			color="white"
-			app
-			flat
-			clipped-left
-			height="80vh"
-			class="d-flex flex-column"
-		>
-			<v-col
-				class="d-flex align-center justify-md-center"
-			>
-				<nuxt-link to="/">
-					<v-img
-						width="105"
-						contain
-						src="/logo_ploggvn.svg"
-					></v-img>
-				</nuxt-link>
-			</v-col>
-			<v-col class="d-md-none align-center justify-end d-flex">
-				<v-app-bar-nav-icon
-					@click="drawer = true"
-				/>
-			</v-col>
+  <v-app id="app" style="overflow: scroll">
+    <v-app-bar
+      elevation="60"
+      elevate-on-scroll
+      color="white"
+      app
+      flat
+      clipped-left
+      height="80vh"
+      class="d-flex flex-column"
+    >
+      <v-col class="d-flex align-center justify-md-center">
+        <nuxt-link to="/">
+          <v-img width="105" contain src="/logo_ploggvn.svg"></v-img>
+        </nuxt-link>
+      </v-col>
+      <v-col class="d-md-none align-center justify-end d-flex">
+        <v-app-bar-nav-icon @click="drawer = true" />
+      </v-col>
 
-			<div class="d-none align-center justify-space-around d-md-flex">
-				
-				<v-btn
-					v-for="(list, index) in lists"
-					:key="index"
-					text
-					plain
-					:href="list.link"
-					:to="localePath(list.link)"
-				>
-					<div class="h7 black--text font-weight-bold text-none">
-						{{ list.title }}
-					</div>
-				</v-btn>
-			</div>
+      <div class="d-none align-center justify-space-around d-md-flex">
+        <v-btn
+          v-for="(list, index) in lists"
+          :key="index"
+          text
+          plain
+          :href="list.link"
+          :to="localePath(list.link)"
+        >
+          <div class="h7 black--text font-weight-bold text-none">
+            {{ list.title }}
+          </div>
+        </v-btn>
+      </div>
 
-			<v-col class="d-md-flex d-none justify-center align-center">
-				<kq-button-locale color="black" />
-			</v-col>
-		</v-app-bar>
-	
-		<v-navigation-drawer
-			v-model="drawer"
-			color="white"
-			fixed
-			right	
-			overflow-hidden
-		>
-			<v-layout column>
-				<v-card height="80" flat color="transparent"></v-card>
-				<v-btn
-					v-for="(list, index) in lists"
-					:key="index"
-					class="my-1"
-					:href="list.link"
-					:to="localePath(list.link)"
-					text
-					height="8vh"
-				>	
-					<div class=" font-weight-bold">
-						{{ list.title }}
-					</div>
-				</v-btn>
-				<kq-button-locale
-					height="10vh"
-					color=" grey"
-				/>
-			</v-layout>
-		</v-navigation-drawer>
-		
-		<v-main class="mt-16 pa-0">
-			<Nuxt />
-		</v-main>
-		<v-footer
-			color="white"
-			class="white--text font-weight-light"
-		>
-			<v-layout column>
-				<v-layout
-					:column="$vuetify.breakpoint.smAndDown"
-					:align-center="$vuetify.breakpoint.lgAndUp"
-					justify-center
-					class="ma-10"
-				>
-					<v-col
-						:cols="
-							$vuetify.breakpoint.smAndDown ? '9 ml-2 pb-10' : '3'
-						"
-					>
-						<v-row v-for="(text, index) in texts" :key="index">
-							<div class="black--text font-weight-bold h6">
-								{{ text.title2 }}
-							</div>
-								<v-img
-									max-width="30"
-									contain
-									:src="text.img"
-								/>
-							<v-col class="black--text">{{ text.title }} </v-col>
-						</v-row>
-					</v-col>
+      <v-col class="d-md-flex d-none justify-center align-center">
+        <kq-button-locale color="black" />
+      </v-col>
+    </v-app-bar>
 
-					<v-col
-						v-for="(head, index) in heads"
-						:key="index"
-						md="2"
-						sm="5"
-						class="d-flex flex-column pb-10 "
-						fill-height
-					>
-						<div class="mb-2 font-weight-bold black--text h7">
-							{{ head.title }} 
-						</div>
-						
-						<div class="black--text">{{ head.subtitle }}</div>
-						<div class="black--text">{{ head.text }}</div>
-						<div class="black--text">{{ head.sub }}</div>
-						
-					</v-col>
-				</v-layout>
+    <v-navigation-drawer
+      v-model="drawer"
+      color="white"
+      fixed
+      right
+      overflow-hidden
+    >
+      <v-layout column>
+        <v-card height="80" flat color="transparent"></v-card>
+        <v-btn
+          v-for="(list, index) in lists"
+          :key="index"
+          class="my-1"
+          :href="list.link"
+          :to="localePath(list.link)"
+          text
+          height="8vh"
+        >
+          <div class="font-weight-bold">
+            {{ list.title }}
+          </div>
+        </v-btn>
+        <kq-button-locale height="10vh" color=" grey" />
+      </v-layout>
+    </v-navigation-drawer>
 
-				<v-divider />
+    <v-main class="mt-16 pa-0">
+      <Nuxt />
+    </v-main>
+    <v-footer color="white" class="white--text font-weight-light">
+      <v-layout column>
+        <v-layout
+          :column="$vuetify.breakpoint.smAndDown"
+          :align-center="$vuetify.breakpoint.lgAndUp"
+          justify-center
+          class="ma-10"
+        >
+          <v-col :cols="$vuetify.breakpoint.smAndDown ? '9 ml-2 pb-10' : '3'">
+            <v-row v-for="(text, index) in texts" :key="index">
+              <div class="black--text font-weight-bold h6">
+                {{ text.title2 }}
+              </div>
+              <v-img max-width="30" contain :src="text.img" />
+              <v-col class="black--text">{{ text.title }} </v-col>
+            </v-row>
+          </v-col>
 
-				<v-col class="d-flex justify-start grey--text"
-					>(C) Plogg Viet Nam 2023</v-col
-				>
-			</v-layout>
-		</v-footer>
-	</v-app>
+          <v-col
+            v-for="(head, index) in heads"
+            :key="index"
+            md="2"
+            sm="5"
+            class="d-flex flex-column pb-10"
+            fill-height
+          >
+            <div class="mb-2 font-weight-bold black--text h7">
+              {{ head.title }}
+            </div>
+
+            <div class="black--text">{{ head.subtitle }}</div>
+            <div class="black--text">{{ head.text }}</div>
+            <div class="black--text">{{ head.sub }}</div>
+          </v-col>
+        </v-layout>
+
+        <v-divider />
+
+        <v-col class="d-flex justify-start grey--text"
+          >(C) Plogg Viet Nam 2023</v-col
+        >
+      </v-layout>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-	data () {
-        return{
-		drawer: false,
-		lists: [
-			{
-				title: this.$t("list.1"),
-				link: "/AboutUs",
-			},
-			{
-				title: this.$t("list.2"),
-				link: "/industries",
-			},
-			{
-				title:this.$t("list.3"),
-				link: "/OurServices",
-			},
-			{
-				title: this.$t("list.4"),
-				link: "/careers",
-			},
-		],
-		texts: [
-			{ title2: this.$t("title2") },
-			{ img: "/Img_Contrach/us1.jpg", title: "Twitter" },
-			{ img: "/Img_Contrach/us2.jpg", title: "Linkedin" },
-			{ img: "/Img_Contrach/us3.jpg", title: "Instagram" },
-			{ img: "/Img_Contrach/us4.jpg", title: "Gmail" },
-			{ img: "/Img_Contrach/us5.jpg", title: this.$t("phone") },
-		],
-		heads: [
-			{
-				title: this.$t("title"),
-				subtitle: this.$t("subtitle"),
-				text: this.$t("careers"),
-				sub:this.$t("sub"),
-			},
-			{ 
-				title: this.$t("title1"),
-				subtitle: this.$t("subtitle1"),
-				sub: this.$t("policy"),
-				text:this.$t("subs"),
-			},
-			{
-				title: this.$t("support"),
-				subtitle: this.$t("feedback"),
-				text: this.$t("help"),
-				sub:this.$t("sub1"),
-			},
-		],
-	};
-},
-	methods: {
-		goToHome() {
-			this.$router.push("/pricing");
-		},
-	},
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+  computed: {
+    lists() {
+      return [
+        {
+          title: this.$t("list.1"),
+          link: "/AboutUs",
+        },
+        {
+          title: this.$t("list.2"),
+          link: "/industries",
+        },
+        {
+          title: this.$t("list.3"),
+          link: "/OurServices",
+        },
+        {
+          title: this.$t("list.4"),
+          link: "/careers",
+        },
+      ];
+    },
+    texts() {
+      return [
+        { title2: this.$t("title2") },
+        { img: "/Img_Contrach/us1.jpg", title: "Twitter" },
+        { img: "/Img_Contrach/us2.jpg", title: "Linkedin" },
+        { img: "/Img_Contrach/us3.jpg", title: "Instagram" },
+        { img: "/Img_Contrach/us4.jpg", title: "Gmail" },
+        { img: "/Img_Contrach/us5.jpg", title: this.$t("phone") },
+      ];
+    },
+    heads() {
+      return [
+        {
+          title: this.$t("title"),
+          subtitle: this.$t("subtitle"),
+          text: this.$t("careers"),
+          sub: this.$t("sub"),
+        },
+        {
+          title: this.$t("title1"),
+          subtitle: this.$t("subtitle1"),
+          sub: this.$t("policy"),
+          text: this.$t("subs"),
+        },
+        {
+          title: this.$t("support"),
+          subtitle: this.$t("feedback"),
+          text: this.$t("help"),
+          sub: this.$t("sub1"),
+        },
+      ];
+    },
+  },
+  methods: {
+    goToHome() {
+      this.$router.push("/pricing");
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 .transition::after {
-	background: white !important;
+  background: white !important;
 }
 .v-app-bar {
-	z-index: 100	
+  z-index: 100;
 }
 </style>
-
 
 <i18n>
 {
